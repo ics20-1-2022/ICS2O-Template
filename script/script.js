9b7ea4c068d7540dddfb08473bd1d8948bb54ada
ics20-1-2022 <113055324+ics20-1-2022@users.noreply.github.com>
2022-10-11 13:51:51 -0400

    add in Javascript function

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+
+function myButtonClicked () {
+    // this function does area and perimeter
+    document.getElementById('some-info').innerHTML = "<p>The answer is: " + (0) + " cm².</p>"
+  }
